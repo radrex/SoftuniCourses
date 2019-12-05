abae88c159e51e8b6e0e1f37e4329c8d9198570c
RadostinV-Stoychev <radostinv.stoychev@gmail.com>
2019-12-05 20:04:34 +0200

    100/100 judge

---
+++ C# Web Developer/C# DB/02.Entity Framework Core/08.JSON Processing/ProductShop/Models/CategoryProduct.cs
@@ -0,0 +1,30 @@
+﻿namespace ProductShop.Models
+{
+    public class CategoryProduct
+    {
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        //------------------------ OVERRIDE --------------------------
+        //public override bool Equals(object obj)
+        //{
+        //    CategoryProduct other = obj as CategoryProduct;
+
+        //    return this.CategoryId == other.CategoryId &&
+        //           this.ProductId == other.ProductId;
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    int hash = 13;
+
+        //    hash = (hash * 7) + this.CategoryId.GetHashCode();
+        //    hash = (hash * 7) + this.ProductId.GetHashCode();
+
+        //    return hash;
+        //}
+    }
+}
