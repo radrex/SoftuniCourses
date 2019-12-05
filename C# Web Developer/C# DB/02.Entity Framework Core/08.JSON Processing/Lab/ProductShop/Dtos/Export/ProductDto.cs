c046d0c0bac953d70c557dba22c1824102ae0db1
RadostinV-Stoychev <radostinv.stoychev@gmail.com>
2019-12-05 20:06:26 +0200

    Clean-up

--- C# Web Developer/C# DB/02.Entity Framework Core/08.JSON Processing/Lab/ProductShop/Dtos/Export/ProductDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductShop.Dtos.Export
-{
-    public class ProductDto
-    {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-        public string Seller { get; set; }
-    }
-}
