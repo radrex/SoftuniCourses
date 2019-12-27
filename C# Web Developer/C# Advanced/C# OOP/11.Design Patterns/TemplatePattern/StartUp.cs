075b4b020b340470716b30bcc3033800511d3c97
RadostinV-Stoychev <radostinv.stoychev@gmail.com>
2019-12-27 23:09:26 +0200

    Design Patterns

---
+++ C# Web Developer/C# Advanced/C# OOP/11.Design Patterns/TemplatePattern/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace TemplatePattern
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+        }
+    }
+}
