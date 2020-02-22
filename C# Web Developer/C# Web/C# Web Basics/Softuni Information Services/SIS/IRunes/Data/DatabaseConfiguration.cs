6f7d5093fbf0ae03c78d11228543fd4363e15ff5
radrex <radostinv.stoychev@gmail.com>
2020-02-22 03:42:08 +0200

    update dbconfig

--- C# Web Developer/C# Web/C# Web Basics/Softuni Information Services/SIS/IRunes/Data/DatabaseConfiguration.cs
+++ C# Web Developer/C# Web/C# Web Basics/Softuni Information Services/SIS/IRunes/Data/DatabaseConfiguration.cs
@@ -2,6 +2,6 @@
 {
     public class DatabaseConfiguration
     {
-        public const string ConnectionString = @"Server=DESKTOP-6UES80A\SQLEXPRESS;Database=IRunes;Trusted_Connection=True;Integrated Security=True;";
+        public const string ConnectionString = @"Server=.;Database=IRunes;Trusted_Connection=True;Integrated Security=True;";
     }
 }
