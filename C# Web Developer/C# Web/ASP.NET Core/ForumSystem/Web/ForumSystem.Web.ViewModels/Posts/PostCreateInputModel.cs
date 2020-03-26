44cc7075b09bb0f266778ce25310f6405fe1a804
radrex <radostinv.stoychev@gmail.com>
2020-03-26 18:19:09 +0200

    Implement CreateForumPost

---
+++ C# Web Developer/C# Web/ASP.NET Core/ForumSystem/Web/ForumSystem.Web.ViewModels/Posts/PostCreateInputModel.cs
@@ -0,0 +1,23 @@
+﻿namespace ForumSystem.Web.ViewModels.Posts
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Services.Mapping;
+
+    public class PostCreateInputModel : IMapTo<Post>
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Range(1, int.MaxValue)]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CategoryDropDownViewModel> Categories { get; set; }
+    }
+}
