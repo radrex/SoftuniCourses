b0cdf79e5ae419d2c3cc3e073fca31acec87c48e
radrex <radostinv.stoychev@gmail.com>
2020-04-11 01:49:09 +0300

    Tests demo

---
+++ C# Web Developer/C# Web/ASP.NET Core/ForumSystem/Tests/ForumSystem.Services.Tests/HomePageTests.cs
@@ -0,0 +1,42 @@
+﻿namespace ForumSystem.Services.Tests
+{
+    using ForumSystem.Web;
+
+    using Microsoft.AspNetCore.Mvc.Testing;
+
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class HomePageTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public HomePageTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public async Task HomePageShouldHaveH1Title()
+        {
+            var serverFactory = new WebApplicationFactory<Startup>();
+            var client = serverFactory.CreateClient();
+            await client.GetAsync("/");
+
+            Stopwatch sw = Stopwatch.StartNew();
+            var response = await client.GetAsync("/");
+            var responseAsString = await response.Content.ReadAsStringAsync();
+            output.WriteLine(sw.Elapsed.ToString());
+            if (sw.Elapsed > new TimeSpan(0, 0, 1))
+            {
+                throw new Exception("Too long wait...");
+            }
+
+            Assert.Contains("<h1", responseAsString);
+        }
+    }
+}
---
+++ C# Web Developer/C# Web/ASP.NET Core/ForumSystem/Tests/ForumSystem.Services.Tests/VotesServiceTests.cs
@@ -0,0 +1,38 @@
+namespace ForumSystem.Services.Tests
+{
+    using ForumSystem.Data;
+    using ForumSystem.Data.Models;
+    using ForumSystem.Services.Data;
+    using ForumSystem.Data.Repositories;
+
+    using Microsoft.EntityFrameworkCore;
+
+    using System;
+    using System.Threading.Tasks;
+
+    using Xunit;
+
+    public class VotesServiceTests
+    {
+        [Fact]
+        public async Task TwoDownVotesShouldCountOnce()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var repository = new EfRepository<Vote>(new ApplicationDbContext(options.Options));
+            var service = new VotesService(repository);
+
+            for (int i = 0; i < 100; i++)
+            {
+                await service.VoteAsync(1, "1", false);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                await service.VoteAsync(1, "2", false);
+            }
+
+            var votes = service.GetVotes(1);
+            Assert.Equal(-2, votes);
+        }
+    }
+}
