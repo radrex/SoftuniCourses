ccd10abc4be600b7ad8ea4b7dc17efe0f7dc4ddb
radrex <radostinv.stoychev@gmail.com>
2020-10-23 06:49:37 +0300

    not done yet

--- JS Web Developer/JS Advanced/JS Advanced/08.Object Composition/Lab/p04_Cars.js
+++ JS Web Developer/JS Advanced/JS Advanced/08.Object Composition/Lab/p04_Cars.js
@@ -1,5 +1,60 @@
 function solve(input) {
+  let cars = [];
 
+  const dataProcessor = function() {
+    return {
+      create: (name) => cars.push({[name]: {}}),
+      createInherit: function(name1, name2) {
+        let car1 = {[name1]: {}};
+        let car2 = cars.find(x => x.hasOwnProperty(name2));
+
+        if (car2 != undefined) {
+          // car1 = {...car1[name1], ...car2[name2]}; // prototypes and inheritance ?
+        }
+        cars.push(car1);
+      },
+      set: function(name, prop, propValue) {
+        let car = cars.find(x => x.hasOwnProperty(name));
+        if (car != undefined) {
+          car[name][prop] = propValue;
+        }
+      },
+      print: function(name) {
+        let car = cars.find(x => x.hasOwnProperty(name));
+        if (car != undefined) {
+          let message = [];
+          Object.entries(car[name]).forEach(([key, value]) => message.push(`${key}:${value}`));
+          console.log(message.join(', '));
+        }
+      }
+    }
+  };
+
+  let processor = dataProcessor();
+
+  for (let arr of input) {
+    let args = arr.split(' ');
+    switch (args[0]) {
+      case 'create':
+        if (args.includes('inherit')) {
+          processor.createInherit(args[1], args[3]);
+        } else {
+          processor.create(args[1]);
+        }
+        break;
+  
+      case 'set':
+        processor.set(args[1], args[2], args[3]);
+        break;
+  
+      case 'print':
+        processor.print(args[1]);
+        break;
+    
+      default:
+        break;
+    }
+  }
 }
 
 solve(['create c1',
