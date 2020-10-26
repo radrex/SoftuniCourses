b515da4fe25b59e1240d3d041bd6d4af989c2d3a
radrex <radostinv.stoychev@gmail.com>
2020-10-26 08:28:33 +0200

    100/100 judge

--- JS Web Developer/JS Advanced/JS Advanced/08.Object Composition/Lab/p04_Cars.js
+++ JS Web Developer/JS Advanced/JS Advanced/08.Object Composition/Lab/p04_Cars.js
@@ -1,59 +1,32 @@
-function solve(input) {
-  let cars = [];
-
-  const dataProcessor = function() {
-    return {
-      create: (name) => cars.push({[name]: {}}),
-      createInherit: function(name1, name2) {
-        let car1 = {[name1]: {}};
-        let car2 = cars.find(x => x.hasOwnProperty(name2));
-
-        if (car2 != undefined) {
-          // car1 = {...car1[name1], ...car2[name2]}; // prototypes and inheritance ?
-        }
-        cars.push(car1);
-      },
-      set: function(name, prop, propValue) {
-        let car = cars.find(x => x.hasOwnProperty(name));
-        if (car != undefined) {
-          car[name][prop] = propValue;
-        }
-      },
-      print: function(name) {
-        let car = cars.find(x => x.hasOwnProperty(name));
-        if (car != undefined) {
-          let message = [];
-          Object.entries(car[name]).forEach(([key, value]) => message.push(`${key}:${value}`));
-          console.log(message.join(', '));
-        }
+function solve(commands) {
+  let map = new Map();
+  let carManager = {
+    create: function ([name, , parent]) {
+      parent = parent ? map.get(parent) : null;
+      let newObj = Object.create(parent);
+      map.set(name, newObj);
+      return newObj;
+    },
+    set: function ([name, key, value]) {
+      let obj = map.get(name);
+      obj[key] = value;
+    },
+    print: function ([name]) {
+      let obj = map.get(name);
+      let allProperties = []
+      Object.keys(obj).forEach(key => allProperties.push(`${key}:${obj[key]}`));
+      while (Object.getPrototypeOf(obj)) {
+        Object.keys(Object.getPrototypeOf(obj)).forEach(key => allProperties.push(`${key}:${Object.getPrototypeOf(obj)[key]}`));
+        obj = Object.getPrototypeOf(obj);
       }
+      console.log(allProperties.join(', '))
     }
   };
 
-  let processor = dataProcessor();
-
-  for (let arr of input) {
-    let args = arr.split(' ');
-    switch (args[0]) {
-      case 'create':
-        if (args.includes('inherit')) {
-          processor.createInherit(args[1], args[3]);
-        } else {
-          processor.create(args[1]);
-        }
-        break;
-  
-      case 'set':
-        processor.set(args[1], args[2], args[3]);
-        break;
-  
-      case 'print':
-        processor.print(args[1]);
-        break;
-    
-      default:
-        break;
-    }
+  for (let cmd of commands) {
+    let tokens = cmd.split(' ');
+    let action = tokens.shift();
+    carManager[action](tokens);
   }
 }
 
